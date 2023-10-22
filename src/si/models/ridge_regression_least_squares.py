be3e52adfda6fff312f7e635d7d12246f4d11dbc
giovannigiofre <giofregiovanni16@gmail.com>
2023-10-22 18:28:29 +0100

    RidgeRegressionLeastSquares

--- src/si/models/ridge_regression_least_squares.py
+++ src/si/models/ridge_regression_least_squares.py
@@ -2,10 +2,155 @@ import numpy as np
 
 from si.data.dataset import Dataset
 from si.metrics.mse import mse
+# from symbol import parameters
 
 
 class RidgeRegressionLeastSquares:
-# implement class
+    """
+    The RidgeRegressionLeastSquares is a linear model using the L2 regularization.
+    This model solves the linear regression problem using the Least Square technique
+
+    Parameters
+    ----------
+    l2_penalty: float
+        The L2 regularization parameter
+    alpha: float
+        The learning rate
+    max_iter: int
+        The maximum number of iterations
+    scale: bool
+        Whether to scale the dataset or not
+
+    Attributes
+    ----------
+    theta: np.array
+        The model parameters, namely the coefficients of the linear model.
+        For example, x0 * theta[0] + x1 * theta[1] + ...
+    theta_zero: float
+        The model parameter, namely the intercept of the linear model.
+        For example, theta_zero * 1
+    """
+
+    def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000,
+                 patience: int = 5, scale: bool = True):
+        """
+
+        Parameters
+        ----------
+        l2_penalty: float
+            The L2 regularization parameter
+        alpha: float
+            The learning rate
+        max_iter: int
+            The maximum number of iterations
+        patience: int
+            The number of iterations without improvement before stopping the training
+        scale: bool
+            Whether to scale the dataset or not
+        """
+        # parameters
+        self.l2_penalty = l2_penalty
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.patience = patience
+        self.scale = scale
+
+        # attributes
+        self.theta = None
+        self.theta_zero = None
+        self.mean = None
+        self.std = None
+
+    def fit(self, dataset: Dataset) -> 'RidgeRegressionLeastSquares':
+        """
+        Fit the model to the dataset
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to fit the model to
+
+        Returns
+        -------
+        self: RidgeRegressionLeastSquares
+            The fitted model
+        """
+        if self.scale:
+            # compute mean and std
+            self.mean = np.nanmean(dataset.X, axis=0)
+            self.std = np.nanstd(dataset.X, axis=0)
+            # scale the dataset
+            X = (dataset.X - self.mean) / self.std
+        else:
+            X = dataset.X
+        
+        y = dataset.y
+        m, n = dataset.shape()
+
+        X_augmented = np.column_stack((np.ones(m), X))
+
+        # Compute the Ridge Regression coefficients using least squares formula
+        I = np.identity(n + 1)
+        I[0, 0] = 0  # Don't regularize the intercept term
+        self.theta = np.linalg.inv(np.dot(X_augmented.T, X_augmented) + self.l2_penalty * I).dot(np.dot(X_augmented.T, dataset.y))
+        self.theta_zero = self.theta[0]
+        self.theta = self.theta[1:]
+        
+        return self
+
+    def predict(self, dataset: Dataset) -> np.array:
+        """
+        Predict the output of the dataset
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to predict the output of
+
+        Returns
+        -------
+        predictions: np.array
+            The predictions of the dataset
+        """
+        X = (dataset.X - self.mean) / self.std if self.scale else dataset.X
+        m = len(dataset.y)
+        X_augmented = np.column_stack((np.ones(m), X))
+        return np.dot(X_augmented, np.concatenate(([self.theta_zero], self.theta)))
+
+
+    def score(self, dataset: Dataset) -> float:
+        """
+        Compute the Mean Square Error of the model on the dataset
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to compute the MSE on
+
+        Returns
+        -------
+        mse: float
+            The Mean Square Error of the model
+        """
+        y_pred = self.predict(dataset)
+        return mse(dataset.y, y_pred)
+
+    def cost(self, dataset: Dataset) -> float:
+        """
+        Compute the cost function (J function) of the model on the dataset using L2 regularization
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to compute the cost function on
+
+        Returns
+        -------
+        cost: float
+            The cost function of the model
+        """
+        y_pred = self.predict(dataset)
+        return (np.sum((y_pred - dataset.y) ** 2) + (self.l2_penalty * np.sum(self.theta ** 2))) / (2 * len(dataset.y))
 
 # This is how you can test it against sklearn to check if everything is fine
 if __name__ == '__main__':
