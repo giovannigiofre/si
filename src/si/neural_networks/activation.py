f2410b9a4c4bf42c6208ad7c3e95b7582157a644
giovannigiofre <giofregiovanni16@gmail.com>
2023-12-07 18:09:03 +0000

    activation functions

--- src/si/neural_networks/activation.py
+++ src/si/neural_networks/activation.py
@@ -178,3 +178,77 @@ class ReLUActivation(ActivationLayer):
             The derivative of the activation function.
         """
         return np.where(input > 0, 1, 0)
+    
+class TanhActivation(ActivationLayer):
+    """
+    Tanh activation function.
+    """
+
+    def activation_function(self, input: np.ndarray):
+        """
+        Tanh activation function.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            The input to the layer.
+
+        Returns
+        -------
+        numpy.ndarray
+            The output of the layer.
+        """
+        return ((np.exp(input))-(np.exp(-input))) / ((np.exp(input))+(np.exp(-input)))
+
+    def derivative(self, input: np.ndarray):
+        """
+        Derivative of the Tanh activation function.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            The input to the layer.
+
+        Returns
+        -------
+        numpy.ndarray
+            The derivative of the activation function.
+        """
+        return 1 - (self.activation_function(input) ** 2)
+    
+class SoftmaxActivation(ActivationLayer):
+    """
+    Softmax activation function.
+    """
+
+    def activation_function(self, input: np.ndarray):
+        """
+        Softmax activation function.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            The input to the layer.
+
+        Returns
+        -------
+        numpy.ndarray
+            The output of the layer.
+        """
+        return np.exp(input) / (sum(np.exp(input)))
+
+    def derivative(self, input: np.ndarray):
+        """
+        Derivative of the Softmax activation function.
+
+        Parameters
+        ----------
+        input: numpy.ndarray
+            The input to the layer.
+
+        Returns
+        -------
+        numpy.ndarray
+            The derivative of the activation function.
+        """
+        return self.activation_function(input) * (1 - self.activation_function(input))
